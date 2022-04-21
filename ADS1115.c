d45ffcb85cf12c71c5f29d15b7e68630935f15d8
Marcelo <marcelosanchez21@gmail.com>
2022-04-21 20:38:24 -0300

    se mejoro el driver al incluir la funcion statusComplete

--- ADS1115.c
+++ ADS1115.c
@@ -1,7 +1,7 @@
-void writeConfig( unsigned int8 i2cAdress = 0, unsigned int16 config = 0 );
-unsigned int16 readConversion( unsigned int8 i2cAdress = 0 );
+void writeConfig( unsigned int16 config = 0 );
+unsigned int16 readConversion( unsigned int16 CHANNEL = 0 );
 
-void writeConfig( unsigned int8 i2cAdress = 0, unsigned int16 config = 0 )
+void writeConfig( unsigned int16 config = 0 )
 {
    unsigned int8 MSB = 0;
    unsigned int8 LSB = 0;
@@ -10,31 +10,94 @@ void writeConfig( unsigned int8 i2cAdress = 0, unsigned int16 config = 0 )
    LSB = config & 0xFF;
    
    i2c_start();
-   i2c_write( i2cAdress | ADDR_WRITE_REGISTER );
+   delay_us( 1 );
+   i2c_write( ADDR_GND | ADDR_WRITE_REGISTER );
    i2c_write( ADDR_POINTER_CONFIGURATION );
    i2c_write( MSB );
    i2c_write( LSB );
    i2c_stop();
-   
-   delay_us( 1000 );
 }
 
-unsigned int16 readConversion( unsigned int8 i2cAdress = 0 )
+unsigned int16 readConversion( unsigned int16 CHANNEL = 0 )
 {
+   unsigned int16 config = 0;
+   
    unsigned int8 MSB = 0;
    unsigned int8 LSB = 0;
    
+   unsigned int16 result;
+   
+   //////////////////////////////////////////////////////////////////////////// CONFIGURATION
+   config = ADDR_CONFIG_OS_TRUE       |
+            ADDR_CONFIG_PGA_1         |
+            ADDR_CONFIG_MODE_SINGLE   |
+            ADDR_CONFIG_DATA_RATE_8   |
+            ADDR_CONFIG_COMP_MODE_1   |
+            ADDR_CONFIG_COMP_POL_1    |
+            ADDR_CONFIG_COMP_LAT_1    |
+            ADDR_CONFIG_COMP_QUE_4;
+            
+   switch( CHANNEL ){
+      case 1:
+         config |= ADDR_CONFIG_MUX_5;
+         break;
+         
+      case 2:
+         config |= ADDR_CONFIG_MUX_6;
+         break;
+         
+      case 3:  
+         config |= ADDR_CONFIG_MUX_7;
+         break;
+         
+      case 4:
+         config |= ADDR_CONFIG_MUX_8;
+         break;
+         
+      default:
+         config |= ADDR_CONFIG_MUX_5;
+         break;
+   }
+   
+   writeConfig( config );
+   delay_us( 30 );           // MUY IMPORTANTE, NO QUITAR.
+   ////////////////////////////////////////////////////////////////////////////
+   
+   //////////////////////////////////////////////////////////////////////////// ¿ CONVERSION COMPLETE ?
+   do{
+      MSB = 0;
+      LSB = 0;
+   
+      i2c_start();
+      delay_us( 1 );
+      i2c_write( ADDR_GND | ADDR_READ_REGISTER );
+      i2c_write( ADDR_POINTER_CONFIGURATION );
+      MSB = i2c_read();
+      LSB = i2c_read();
+      i2c_stop();
+   }while( ( make16( MSB, LSB ) & 0x8000 ) == 0 );
+   ////////////////////////////////////////////////////////////////////////////
+   
    i2c_start();
-   i2c_write( i2cAdress | ADDR_WRITE_REGISTER );
+   delay_us( 1 );
+   i2c_write( ADDR_GND | ADDR_WRITE_REGISTER );
    i2c_write( ADDR_POINTER_CONVERSION );
    i2c_stop();
    
+   MSB = 0;
+   LSB = 0;
+   
    i2c_start();
-   i2c_write( i2cAdress | ADDR_READ_REGISTER );
+   delay_us( 1 );
+   i2c_write( ADDR_GND | ADDR_READ_REGISTER );
    MSB = i2c_read();
    LSB = i2c_read();
-   i2c_write( ADDR_POINTER_CONVERSION );
-   i2c_stop();
+   i2c_stop();  
    
-   return make16( MSB, LSB );
+   result = 0;
+   result = make16( MSB, LSB ) >> 4;
+   
+   //if( result > 4000 ){ result = 0; } // filter.
+   
+   return result;
 }
